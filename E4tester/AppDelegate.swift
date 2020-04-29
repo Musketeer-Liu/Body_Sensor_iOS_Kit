fc251ed2b38e6b134c05bf8045be919cb5c047b5
Musketeer.Liu <Musketeer.Liu@gmail.com>
2020-04-29 00:11:43 -0700

    Add E4 Structure

---
+++ E4tester/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  E4 tester
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+        EmpaticaAPI.prepareForBackground()
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+        EmpaticaAPI.prepareForResume()
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+}
+
